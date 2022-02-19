1093c815aa6338fcc8249fb8f4f6ba85b5033c32
yashyadav <yakuyadav19@gmail.com>
2022-02-19 20:33:02 +0530

    Addedtogglebutton

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -3,6 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    addAndMakeVisible(toggleButton); 
     setSize (600, 400);
 }
 
@@ -23,6 +24,7 @@ void MainComponent::paint (juce::Graphics& g)
 
 void MainComponent::resized()
 {
+    toggleButton.setBounds(10, 10, 30, 30); 
     // This is called when the MainComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
